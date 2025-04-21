e3e74b338de1ef358ee327c21151fbe915f55648
Jan <hansafunc@gmail.com>
2025-04-21 18:51:23 +0800

    fix

---
+++ testBacktest.py
@@ -0,0 +1,42 @@
+from freqtrade.commands import (
+start_backtesting,
+start_trading,
+start_webserver
+)
+
+from freqtrade.commands import Arguments
+from freqtrade.main import main
+def get_args(args):
+    return Arguments(args).get_parsed_arg()
+
+CURRENT_TEST_STRATEGY = "FreqaiExampleStrategy"
+
+def trade():
+    args = ["trade",
+            "--config",
+            "freqai_config.json",
+            "--strategy",
+            CURRENT_TEST_STRATEGY,
+            "--freqaimodel",
+            "LightGBMClassifier"]
+    # start_webserver(get_args(args))
+    # start_trading(get_args(args))
+    main(args)
+
+def backtesing():
+
+    args = ["backtesting",
+            "--config",
+            "freqai_config.json",
+            "--strategy",
+            CURRENT_TEST_STRATEGY,
+            "--timeframe",
+            "15m"
+           ]
+    # start_webserver(get_args(args))
+    # start_trading(get_args(args))
+    main(args)
+
+
+if __name__ == "__main__":
+    backtesing()
